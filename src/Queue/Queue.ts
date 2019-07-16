fa0f52f23f4ba4b0ab43e72b8a889c1de4f4a952
Avinash Nishad <js.avinash.nishad@gmail.com>
2019-07-16 15:22:46 +0530

    Queue added

--- src/Queue/Queue.ts
+++ src/Queue/Queue.ts
@@ -1,25 +1,100 @@
+export type nodeType<T> = IQueueNode<T> | null;
+
 export interface IQueue<T> {
     enqueue(data: T): void;
     denqueue(): void;
+    display(): void;
+    peak(): T | null;
+}
+export interface IQueueNode<T> {
+    setNext(next: nodeType<T>): void;
+    setData(data: T): void;
+    getData(): T;
+    getNext(): nodeType<T>;
+}
+
+export class QueueNode<T> implements IQueueNode<T> {
+    protected data: T;
+    protected next: nodeType<T>;
+    constructor(data: T, next: nodeType<T>) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public setNext(next: nodeType<T>) {
+        this.next = next;
+    }
+
+    public setData(data: T) {
+        this.data = data;
+    }
+
+    public getNext(): nodeType<T> {
+        return this.next;
+    }
+
+    public getData(): T {
+        return this.data;
+    }
 }
 export class Queue<T> implements IQueue<T> {
-    private data: T[] = [];
+    private front: nodeType<T> = null;
+    private rear: nodeType<T> = null;
+    private size: number = 0;
 
-    public enqueue = (data: T) => {
-        this.data.push(data);
+    public isEmpty = (): boolean => {
+        return this.front == null;
+    }
+
+    public clear = (): void => {
+        this.front = null;
+        this.rear = null;
+        this.size = 0;
+    }
+
+    public enqueue = (data: T): void => {
+        const newNode: nodeType<T> = new QueueNode<T>(data, null);
+        this.size++;
+        if (this.front == null) {
+            this.front = newNode;
+            this.rear = this.front;
+        }
+        else {
+            if (newNode) newNode.setNext(this.front);
+            this.rear = newNode;
+        }
     }
 
     public denqueue = () => {
-        this.data.shift();
+        const newNode: nodeType<T> = this.front;
+        this.size++;
+        if (this.front == null) {
+            this.front = newNode;
+            this.rear = this.front;
+        }
+        else {
+            if (newNode) newNode.setNext(this.front);
+            this.front = newNode;
+        }
     }
-    public peak = (): T => {
-        return this.data[this.data.length - 1];
+    public peak = (): T | null => {
+        return this.rear && this.rear.getData();
     }
 
-    public display = (): void => {
-        console.log('Queue :');
-        this.data.map((element: T) => {
-            console.log(`${element} `);
-        })
+    public display = () => {
+        console.log('Dequeue :');
+        if (this.size == 0) {
+            console.log('Empty :');
+            return;
+        }
+        let newNode: nodeType<T> = this.front;
+        if (newNode && this.rear) {
+            while (newNode != this.rear.getNext()) {
+                if (newNode) {
+                    console.log(`${newNode.getData()} `);
+                    newNode = newNode.getNext();
+                }
+            }
+        }
     }
 }
